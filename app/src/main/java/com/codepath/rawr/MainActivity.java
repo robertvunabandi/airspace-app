c8fb1bc7de95dc2765ee402647e3c94058b74ce4
Robert Vunabandi <robertvunabandi@fb.com>
2017-07-25 13:13:12 -0700

    Changes to send receive

--- app/src/main/java/com/codepath/rawr/MainActivity.java
+++ app/src/main/java/com/codepath/rawr/MainActivity.java
@@ -8,9 +8,9 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.codepath.rawr.adapters.MainPagerAdapter;
+import com.codepath.rawr.fragments.SendReceiveFragment;
 
 public class MainActivity extends AppCompatActivity {
     // setting up the vire pager for fragments
@@ -42,9 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO - set the images of the fragments
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_android);
-
-        vpPager.setCurrentItem(1);
-        Snackbar.make(parentLayout, "Welcome!", Snackbar.LENGTH_LONG).show();
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_android);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_android);
     }
 
 
@@ -54,15 +53,15 @@ public class MainActivity extends AppCompatActivity {
         // TODO - this snackbar is not displaying, fix this
         if (resultCode == RESULT_OK && requestCode == CODE_SENDER_FORM_ACTIVITY) {
             // success snackbar
-            Log.e(TAG, "Snackbar should be show in success");
             Snackbar.make(parentLayout, "Your request has been sent.", Snackbar.LENGTH_LONG).show();
             // TODO - uncomment the line below and implement "clearViews()" and "refreshPending()" methods
-            // ((SendReceiveFragment) pagerAdapter.getItem(vpPager.getCurrentItem())).clearViews();
-            // ((SendReceiveFragment) pagerAdapter.getItem(vpPager.getCurrentItem())).refreshPending();
+            ((SendReceiveFragment) pagerAdapter.getItem(vpPager.getCurrentItem())).clearViews();
+            ((SendReceiveFragment) pagerAdapter.getItem(vpPager.getCurrentItem())).refreshRequests();
         } else if (resultCode == RESULT_CANCELED && requestCode == CODE_SENDER_FORM_ACTIVITY) {
             // failure snackbar
-            Log.e(TAG, "Snackbar should be show in error");
-            Snackbar.make(parentLayout, String.format("THE FOLLOWING ERROR OCCURRED: %s", data.getStringExtra("message")), Snackbar.LENGTH_LONG).show();
+            Snackbar.make(parentLayout, String.format("THE FOLLOWING ERROR OCCURRED: %s", data.getStringExtra("message")), Snackbar.LENGTH_INDEFINITE).show();
+            // clear the texts so that we're back to nothing
+            ((SendReceiveFragment) pagerAdapter.getItem(vpPager.getCurrentItem())).clearViews();
         }
     }
 }
