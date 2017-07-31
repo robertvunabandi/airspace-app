0872499324cfcbdc654a9aae42c2094d8a3dbaac
Amanda Lee <mandaleeyp@MacBook-Pro-8.dhcp.thefacebook.com>
2017-07-31 16:14:28 -0700

    Adding accepted reqs

--- app/src/main/java/com/codepath/rawr/TravelAcceptedRequestsActivity.java
+++ app/src/main/java/com/codepath/rawr/TravelAcceptedRequestsActivity.java
@@ -1,9 +1,26 @@
 package com.codepath.rawr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import com.codepath.rawr.adapters.TravelAcceptedRequestsAdapter;
+import com.codepath.rawr.models.ShippingRequest;
+import com.loopj.android.http.AsyncHttpClient;
+
+import java.util.ArrayList;
 
 public class TravelAcceptedRequestsActivity extends AppCompatActivity {
+
+    public static String TAG = "Accepted Req";
+    AsyncHttpClient client;
+    public String[] DB_URLS;
+    public String travelNoticeId;
+
+    // Declaring variables for list of pending requests
+    TravelAcceptedRequestsAdapter travelAcceptedRequestsAdapter;
+    ArrayList<ShippingRequest> mRequests;
+    RecyclerView rv_pending_requests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
